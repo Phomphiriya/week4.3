6c17d4da533b602a12817dab1324c6a933be784d
Phomphiriya <Phomphiriya@LAPTOP-36D1T06O>
2020-09-07 00:04:00 +0700

    Add project files.

---
+++ week4.3/Source.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int L,W,x;
+	printf("Find area of square");
+	printf("\nEnter Length :");
+	scanf_s("%d", &L);
+	printf("Enter Width :");
+	scanf_s("%d", &W);
+	x = L * W;
+		printf("%d", x);
+	return 0;
+}
